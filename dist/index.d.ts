1ec01478106a97a507700fa4fcffeb0fcb28de1d
Steven Toninelli <steven.toninelli03@gmail.com>
2024-04-02 16:46:21 +0200

    feat: published on npm

---
+++ dist/index.d.ts
@@ -0,0 +1,197 @@
+/// <reference types="node" />
+export declare enum Log_Level {
+    TRACKLE_TRACE = 1,
+    TRACKLE_INFO = 30,
+    TRACKLE_WARN = 40,
+    TRACKLE_ERROR = 50,
+    TRACKLE_PANIC = 60,
+    TRACKLE_NO_LOG = 70
+}
+export declare enum Ota_Method {
+    NO_OTA = 0,
+    PUSH = 1,
+    SEND_URL = 2
+}
+export declare enum Connection_Type {
+    CONNECTION_TYPE_UNDEFINED = 0,
+    CONNECTION_TYPE_WIFI = 1,
+    CONNECTION_TYPE_ETHERNET = 2,
+    CONNECTION_TYPE_CELLULAR = 3,
+    CONNECTION_TYPE_LPWA = 4
+}
+export declare enum Function_PermissionDef {
+    ALL_USERS = 1,
+    OWNER_ONLY = 2
+}
+export declare enum Data_TypeDef {
+    VAR_BOOLEAN = 1,
+    VAR_INT = 2,
+    VAR_STRING = 4,
+    VAR_CHAR = 5,
+    VAR_LONG = 6,
+    VAR_JSON = 7,
+    VAR_DOUBLE = 9
+}
+export declare enum Event_Type {
+    PUBLIC = "e",
+    PRIVATE = "E"
+}
+export declare enum Event_Flags {
+    EMPTY_FLAGS = 0,
+    NO_ACK = 2,
+    WITH_ACK = 8,
+    ALL_FLAGS = 10
+}
+/**
+ * Represents the TrackleWrapper interface.
+ */
+interface TrackleWrapper {
+    Log_Level: any;
+    Function_PermissionDef: any;
+    Data_TypeDef: any;
+    /**
+     * Gets the maximum length of the device ID.
+     * @returns The maximum length of the device ID.
+     */
+    getMaxDeviceIdLength(): number;
+    /**
+     * Gets the maximum length of the device private key.
+     * @returns The maximum length of the device private key.
+     */
+    getMaxDevicePrivateKeyLength(): number;
+    /**
+     * Sets the log callback function.
+     * @param callback - The log callback function.
+     */
+    setLogCallback(callback: (msg: string, level: number, category: string, attribute?: Buffer, reserved?: Buffer) => void): void;
+    /**
+     * Sets the log level.
+     * @param logLevel - The log level.
+     */
+    setLogLevel(logLevel: Log_Level): void;
+    /**
+     * Sets the enabled status.
+     * @param status - The enabled status.
+     */
+    setEnabled(status: boolean): void;
+    /**
+     * Sets the firmware version.
+     * @param firmwareVersion - The firmware version.
+     */
+    setFirmwareVersion(firmwareVersion: number): void;
+    /**
+     * Sets the OTA method.
+     * @param method - The OTA method.
+     */
+    setOtaMethod(method: Ota_Method): void;
+    /**
+     * Sets the connection type.
+     * @param connType - The connection type.
+     */
+    setConnectionType(connType: Connection_Type): void;
+    /**
+     * Sets the millis callback function.
+     * @param callback - The millis callback function.
+     */
+    setMillis(callback: () => number): void;
+    /**
+     * Sets the device ID.
+     * @param deviceId - The device ID.
+     */
+    setDeviceId(deviceId: Uint8Array): void;
+    /**
+     * Sets the device keys.
+     * @param deviceKey - The device keys.
+     */
+    setKeys(deviceKey: Uint8Array): void;
+    /**
+     * Sets the send callback function.
+     * @default // With no parameters, the function will be called with the default c++ libraries.
+     * @param callback - The send callback function.
+     * @returns The result of the operation.
+     */
+    setSendCallback(callback?: (buffer: Buffer, bufferLen: number, tmp: any) => number | Promise<number>): number;
+    /**
+     * Sets the receive callback function.
+     * @default // With no parameters, the function will be called with the default c++ libraries.
+     * @param callback - The receive callback function.
+     * @returns The result of the operation.
+     */
+    setReceiveCallback(callback?: (buffer: Buffer, bufferLen: number, tmp: any) => number | Promise<number>): number;
+    /**
+     * Sets the connect callback function.
+     * @default // With no parameters, the function will be called with the default c++ libraries.
+     * @param callback - The connect callback function.
+     * @returns The result of the operation.
+     */
+    setConnectCallback(callback?: (address: string, port: number) => number | Promise<number>): number;
+    /**
+     * Sets the disconnect callback function.
+     * @default // With no parameters, the function will be called with the default c++ libraries.
+     * @param callback - The disconnect callback function.
+     * @returns The result of the operation.
+     */
+    setDisconnectCallback(callback?: () => number | Promise<number>): number;
+    /**
+     * Sets the system time callback function.
+     * @param callback - The system time callback function.
+     */
+    setSystemTimeCallback(callback: (time: number, param: number, data?: Buffer) => void): void;
+    /**
+     * Sets the system reboot callback function.
+     * @param callback - The system reboot callback function.
+     */
+    setSystemRebootCallback(callback: (data: string) => void): void;
+    /**
+     * Sets the publish health check interval.
+     * @param interval - The publish health check interval.
+     */
+    setPublishHealthCheckInterval(interval: number): void;
+    /**
+     * Sets the completed publish callback function.
+     * @param callback - The completed publish callback function.
+     */
+    setCompletedPublishCallback(callback: (error: number, callbackData?: number) => void): void;
+    /**
+     * Connects to the server.
+     * @returns The result of the operation.
+     */
+    connect(): number;
+    /**
+     * Disconnects from the server.
+     */
+    disconnect(): void;
+    /**
+     * Performs the main loop.
+     */
+    loop(): void;
+    /**
+     * Posts a function to the server.
+     * @param callbackName - The name of the callback function.
+     * @param callback - The callback function.
+     * @param permission - The function permission definition.
+     * @returns A boolean indicating if the operation was successful.
+     */
+    post(callbackName: string, callback: (arg: string, args?: []) => number, permission: Function_PermissionDef): boolean;
+    /**
+     * Gets a function from the server.
+     * @param callbackName - The name of the callback function.
+     * @param callback - The callback function.
+     * @param returnType - The data type of the return value.
+     * @returns A boolean indicating if the operation was successful.
+     */
+    get(callbackName: string, callback: (arg: string) => any, returnType: Data_TypeDef): boolean;
+    /**
+     * Publishes an event to the server.
+     * @param eventName - The name of the event.
+     * @param data - The data to be published.
+     * @param ttl - The time-to-live of the event.
+     * @param eventType - The type of the event.
+     * @param eventFlags - The flags of the event.
+     * @param msgKey - The message key.
+     * @returns A boolean indicating if the operation was successful.
+     */
+    publish(eventName: string, data: string, ttl?: number, eventType?: Event_Type, eventFlags?: Event_Flags, msgKey?: number): boolean;
+}
+declare const proxy: TrackleWrapper;
+export default proxy;
---
+++ dist/index.js
@@ -0,0 +1,57 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Event_Flags = exports.Event_Type = exports.Data_TypeDef = exports.Function_PermissionDef = exports.Connection_Type = exports.Ota_Method = exports.Log_Level = void 0;
+const trackle = require("bindings")("trackle_wrapper.node");
+var Log_Level;
+(function (Log_Level) {
+    Log_Level[Log_Level["TRACKLE_TRACE"] = 1] = "TRACKLE_TRACE";
+    Log_Level[Log_Level["TRACKLE_INFO"] = 30] = "TRACKLE_INFO";
+    Log_Level[Log_Level["TRACKLE_WARN"] = 40] = "TRACKLE_WARN";
+    Log_Level[Log_Level["TRACKLE_ERROR"] = 50] = "TRACKLE_ERROR";
+    Log_Level[Log_Level["TRACKLE_PANIC"] = 60] = "TRACKLE_PANIC";
+    Log_Level[Log_Level["TRACKLE_NO_LOG"] = 70] = "TRACKLE_NO_LOG";
+})(Log_Level || (exports.Log_Level = Log_Level = {}));
+var Ota_Method;
+(function (Ota_Method) {
+    Ota_Method[Ota_Method["NO_OTA"] = 0] = "NO_OTA";
+    Ota_Method[Ota_Method["PUSH"] = 1] = "PUSH";
+    Ota_Method[Ota_Method["SEND_URL"] = 2] = "SEND_URL";
+})(Ota_Method || (exports.Ota_Method = Ota_Method = {}));
+var Connection_Type;
+(function (Connection_Type) {
+    Connection_Type[Connection_Type["CONNECTION_TYPE_UNDEFINED"] = 0] = "CONNECTION_TYPE_UNDEFINED";
+    Connection_Type[Connection_Type["CONNECTION_TYPE_WIFI"] = 1] = "CONNECTION_TYPE_WIFI";
+    Connection_Type[Connection_Type["CONNECTION_TYPE_ETHERNET"] = 2] = "CONNECTION_TYPE_ETHERNET";
+    Connection_Type[Connection_Type["CONNECTION_TYPE_CELLULAR"] = 3] = "CONNECTION_TYPE_CELLULAR";
+    Connection_Type[Connection_Type["CONNECTION_TYPE_LPWA"] = 4] = "CONNECTION_TYPE_LPWA";
+})(Connection_Type || (exports.Connection_Type = Connection_Type = {}));
+var Function_PermissionDef;
+(function (Function_PermissionDef) {
+    Function_PermissionDef[Function_PermissionDef["ALL_USERS"] = 1] = "ALL_USERS";
+    Function_PermissionDef[Function_PermissionDef["OWNER_ONLY"] = 2] = "OWNER_ONLY";
+})(Function_PermissionDef || (exports.Function_PermissionDef = Function_PermissionDef = {}));
+var Data_TypeDef;
+(function (Data_TypeDef) {
+    Data_TypeDef[Data_TypeDef["VAR_BOOLEAN"] = 1] = "VAR_BOOLEAN";
+    Data_TypeDef[Data_TypeDef["VAR_INT"] = 2] = "VAR_INT";
+    Data_TypeDef[Data_TypeDef["VAR_STRING"] = 4] = "VAR_STRING";
+    Data_TypeDef[Data_TypeDef["VAR_CHAR"] = 5] = "VAR_CHAR";
+    Data_TypeDef[Data_TypeDef["VAR_LONG"] = 6] = "VAR_LONG";
+    Data_TypeDef[Data_TypeDef["VAR_JSON"] = 7] = "VAR_JSON";
+    Data_TypeDef[Data_TypeDef["VAR_DOUBLE"] = 9] = "VAR_DOUBLE";
+})(Data_TypeDef || (exports.Data_TypeDef = Data_TypeDef = {}));
+var Event_Type;
+(function (Event_Type) {
+    Event_Type["PUBLIC"] = "e";
+    Event_Type["PRIVATE"] = "E";
+})(Event_Type || (exports.Event_Type = Event_Type = {}));
+var Event_Flags;
+(function (Event_Flags) {
+    Event_Flags[Event_Flags["EMPTY_FLAGS"] = 0] = "EMPTY_FLAGS";
+    Event_Flags[Event_Flags["NO_ACK"] = 2] = "NO_ACK";
+    Event_Flags[Event_Flags["WITH_ACK"] = 8] = "WITH_ACK";
+    Event_Flags[Event_Flags["ALL_FLAGS"] = 10] = "ALL_FLAGS";
+})(Event_Flags || (exports.Event_Flags = Event_Flags = {}));
+const handler = {};
+const proxy = new Proxy(trackle, handler);
+exports.default = proxy;
